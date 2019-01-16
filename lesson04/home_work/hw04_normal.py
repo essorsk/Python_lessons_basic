6b2cbe64f4fe05528db81cf9aeff14bf281b3a76
essorsk <essor-sk@yandex.ru>
2019-01-16 21:55:35 +0700

    Lesson 04

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,9 +1,11 @@
+__author__ = 'Козина Светлана Владимировна'
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+print('\nЗадание-1: вывод малышей')
+import re
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -19,7 +21,36 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+sm1 = re.findall('([a-z]+)',line)
+print('\nWith re: \n',sm1)
 
+# Without re
+abc = 'abcdefghijklmnopqrstuvwxyz'
+mline = line[:]
+fline = ''
+list = []
+k = 0
+i = 0
+while len(mline) > 0:
+    while i < len(mline) and mline[i] in abc:
+        k += 1
+        i += 1
+    if k > 1:
+        for j in range(k):
+            fline += mline[j]
+        list.append(fline)
+        fline = ''
+        k = 0
+        mline = mline[i:]
+        i = 0
+    elif k == 1:
+        list.append(mline[0])
+        k = 0
+        mline = mline[1:]
+        i = 0
+    else:
+        mline = mline[1:]
+print('\nWithout re:\n',list)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,7 +59,8 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
-
+print('\nЗадание-2:')
+import re
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
        'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
@@ -45,9 +77,57 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+result = re.findall('[a-z][a-z]([A-Z]+)[A-Z][A-Z]',line_2)
+print('\nWith re: \n',result)
+
+# Without re
+abc = "abcdefghijklmnopqrstuvwxyz"
+xyz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+fline = ""
+list = []
+b = 0
+while len(line_2) > 4:
+    if line_2[2] in xyz and line_2[1] in abc and line_2[0] in abc and line_2[3] in xyz and line_2[4] in xyz:
+        line_2 = line_2[2:]
+        while line_2[b] in xyz:
+            fline += line_2[b]
+            b += 1
+        fline = fline[:-2]
+        list.append(fline)
+        fline = ''
+        line_2 = line_2[b:]
+        b = 0
+    else:
+        line_2 = line_2[1:]
+print('\nWithout re: \n',list)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print('\nЗадание-3: 2500 & maxlenght')
+import os
+import random
+import re
+
+name = 'hw04_2500'
+new_path = os.path.join(name)
+file = open(new_path,'w')
+for i in range(0,2500):
+    file.write(str(random.randint(0,9)))
+file.close()
+
+file = open(new_path,'r')
+pattern = '1{2,2500}|2{2,2500}|3{2,2500}|4{2,2500}|5{2,2500}|6{2,2500}|7{2,2500}|8{2,2500}|9{2,2500}|0{2,2500}'
+result = re.findall(pattern,file.readline())
+file.close()
+
+max = 0
+length = ''
+for j in result:
+    if len(j) > max:
+        length = j[:]
+        max = len(j)
+print('\nМаксимальная последовательность в 2500 файле: ',length)
